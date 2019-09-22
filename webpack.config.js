a6c8a5ae9ee632f36417f883f2776c05b74dd0c3
Khang Hoang <hoangtrieukhang@gmail.com>
2019-09-22 14:11:35 -0700

    not working yet

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/client/index.js',
+  mode: "production",
+  entry: "./src/client/index.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: 'Foo',
-    libraryTarget: 'window'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    library: "Foo",
+    libraryTarget: "window"
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
-    publicPath: '/dist/'
+    publicPath: "/dist/"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts"]
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.ts$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader'
+          loader: "ts-loader"
         }
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
       }
     ]
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM"
   }
 };
